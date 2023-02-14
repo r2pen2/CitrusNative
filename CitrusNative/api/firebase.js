9d621608530ba252761fbc87b1b24e242341ec04
Joe Dobbelaar <joedobbelaar@gmail.com>
2023-02-14 09:34:21 -0600

    Added DB Manager + reworked GroupManager

--- CitrusNative/api/firebase.js
+++ CitrusNative/api/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { doc, collection, addDoc, getDoc, setDoc, deleteDoc, getFirestore } from "firebase/firestore";
+import { getAuth, signOut, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const firestore = getFirestore();
+export const auth = getAuth(app);
