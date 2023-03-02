2c883311987a7a5cc9b4ec4277d1b50328a04968
Joe Dobbelaar <joedobbelaar@gmail.com>
2023-03-02 08:52:39 -0600

    Clicking name in topbar should bring user to settings (not just the avatar)

--- CitrusNative/components/Topbar.js
+++ CitrusNative/components/Topbar.js
@@ -59,12 +59,10 @@ export default function Topbar({nav, onNotificationClick}) {
   // As long as there is a currentUserManager, return the topbar
   return currentUserManager && (
     <View display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" style={{paddingTop: 10, paddingLeft: 20, paddingRight: 20}}>
-        <View display="flex" flexDirection="row" alignItems="center" >
-          <Pressable onPress={() => nav.navigate("settings")} >
-            <AvatarIcon src={currentUserManager.data.personalData.pfpUrl} onClick={() => nav.navigate("settings")} />
-          </Pressable>
-            <StyledText text={currentUserManager.data.personalData.displayName} alignItems="flex-start" marginLeft={10}/>
-        </View>
+        <Pressable onPress={() => nav.navigate("settings")} display="flex" flexDirection="row" alignItems="center">
+          <AvatarIcon src={currentUserManager.data.personalData.pfpUrl} onClick={() => nav.navigate("settings")} />
+          <StyledText text={currentUserManager.data.personalData.displayName} alignItems="flex-start" marginLeft={10}/>
+        </Pressable>
         <Pressable onPress={onNotificationClick}>
           <Image source={dark ? require('../assets/images/NotificationIcon.png') : require('../assets/images/NotificationIconLight.png')} style={{width: 32, height: 32}} />
             { hasUnreadNotifications() && <NotificationDot /> }
