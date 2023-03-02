09c0fb6f751816298336160cdecca06f9904cccd
Joe Dobbelaar <joedobbelaar@gmail.com>
2023-03-02 08:49:58 -0600

    Settings.js doc

--- CitrusNative/navigation/Settings.js
+++ CitrusNative/navigation/Settings.js
@@ -1,14 +1,36 @@
-import { useContext } from "react";
-import { View } from "react-native";
-import {AvatarIcon} from "../components/Avatar";
-import { CenteredTitle } from "../components/Text";
-import { SettingsWrapper } from "../components/Wrapper";
-import { DarkModeButton, StyledButton, EditButton } from "../components/Button";
-import { CurrentUserContext, GroupsContext, ListenedGroupsContext, ListenedTransactionsContext, ListenedUsersContext, TransactionsContext, UnsubscribeCurrentUserContext, UsersContext } from "../Context";
-import { googleAuth } from "../api/auth";
+// Library Imports
+import { useContext, } from "react";
+import { View, } from "react-native";
 
+// Component Imports
+import { AvatarIcon, } from "../components/Avatar";
+import { DarkModeButton, EditButton, StyledButton, } from "../components/Button";
+import { CenteredTitle, } from "../components/Text";
+import { SettingsWrapper, } from "../components/Wrapper";
+
+// Context Imports
+import { 
+  CurrentUserContext, 
+  GroupsContext, 
+  ListenedGroupsContext, 
+  ListenedTransactionsContext, 
+  ListenedUsersContext, 
+  TransactionsContext, 
+  UnsubscribeCurrentUserContext, 
+  UsersContext,
+} from "../Context";
+
+// API Imports
+import { googleAuth, } from "../api/auth";
+
+/**
+ * Component for showing current user data and settings. Has an option to sign-out which whipes the 
+ * app's local data.
+ * @param {ReactNavigation} navigation navigation object from main app shell
+ */
 export default function Settings({navigation}) {
 
+  // Get contexts (super annoying because we really just need the setters lol)
   const { currentUserManager, setCurrentUserManager } = useContext(CurrentUserContext);
   const { usersData, setUsersData } = useContext(UsersContext);
   const { listenedUsers, setListenedUsers } = useContext(ListenedUsersContext);
@@ -18,18 +40,23 @@ export default function Settings({navigation}) {
   const { listenedTransactions, setListenedTransactions } = useContext(ListenedTransactionsContext);
   const { unsubscribeCurrentUser } = useContext(UnsubscribeCurrentUserContext);
 
+  /**
+   * Sign out the current user and wipe all context
+   * @async
+   */
   async function handleLogout() {
-    await googleAuth.signOut();
-    setUsersData({});
-    setListenedUsers([]);
-    setGroupsData({});
-    setListenedGroups([]);
-    setTransactionsData({});
-    setListenedTransactions([]);
-    unsubscribeCurrentUser();
-    setCurrentUserManager(null);
+    await googleAuth.signOut();   // Sign out user from Google Authenticaion session
+    setUsersData({});             // Wipe users data
+    setListenedUsers([]);         // Wipe listened users array
+    setGroupsData({});            // Wipe groups data
+    setListenedGroups([]);        // Wipe listened groups array
+    setTransactionsData({});      // Wipe transaction data
+    setListenedTransactions([]);  // Wipe listened transcations array
+    unsubscribeCurrentUser();     // Unsubscribe the current user manager from updates! (jesus christ this took me forever to figure out)
+    setCurrentUserManager(null);  // Set the current user manager to null so that we can login again
   }
 
+  // Render the settings page so long as there is a currentUserManager
   return currentUserManager && (
     <SettingsWrapper>
       <View 
@@ -43,29 +70,27 @@ export default function Settings({navigation}) {
           elevation: 2,
           paddingBottom: 50,
           paddingTop: 10,
-        }}>
-
-          <AvatarIcon src={currentUserManager.data.personalData.pfpUrl} size={200} />
-          
-          <View 
-            display="flex" 
-            flexDirection="row" 
-            justifyContent="space-between" 
-            alignItems="center" 
-            style={{
-              width: "50%",
-              margin: 20
-            }}>
-              <EditButton />
-              <DarkModeButton />
-          </View>
-          
-          <CenteredTitle text={currentUserManager.data.personalData.displayName} />
-          <CenteredTitle text={"Email: " + (currentUserManager.data.personalData.email ? currentUserManager.data.personalData.email : "?")} alignment="left" />
-          <CenteredTitle text={"Phone: " + (currentUserManager.data.personalData.phoneNumber ? currentUserManager.data.personalData.phoneNumber : "?")} alignment="left" />
-
-          <StyledButton text="Logout" color="red" onClick={handleLogout}/>
+        }}
+      >
+        <AvatarIcon src={currentUserManager.data.personalData.pfpUrl} size={200} />
+        <View 
+          display="flex" 
+          flexDirection="row" 
+          justifyContent="space-between" 
+          alignItems="center" 
+          style={{
+            width: "50%",
+            margin: 20
+          }}
+        >
+          <EditButton />
+          <DarkModeButton />
+        </View>
+        <CenteredTitle text={currentUserManager.data.personalData.displayName} />
+        <CenteredTitle text={"Email: " + (currentUserManager.data.personalData.email ? currentUserManager.data.personalData.email : "?")} alignment="left" />
+        <CenteredTitle text={"Phone: " + (currentUserManager.data.personalData.phoneNumber ? currentUserManager.data.personalData.phoneNumber : "?")} alignment="left" />
+        <StyledButton text="Logout" color="red" onClick={handleLogout}/>
       </View>
     </SettingsWrapper>
-  )
+  );
 }
